61333a9d631aecbd4751082a3ce9028008648a8a
Natan <natan.santolo@gmail.com>
2011-12-16 12:21:20 +0100

    Debugin

--- public/js/sources/NowAndNextSource.js
+++ public/js/sources/NowAndNextSource.js
@@ -9,7 +9,7 @@ define([
 
 function(Model, Collection){
 
-	var source = 'http://tvgids.upc.nl/scheduleApi/api/Channel/7J%7C6s%7C7G%7C7K%7C7L/events/NowAndNext.json?optionalProperties=Channel.url%2CChannel.logoIMG%2CEvent.url&order=startDateTime';//&batchSize=2&batch=0';
+	var source = 'http://tvgids.upc.nl/scheduleApi/api/Channel/7J%7C6s%7C7G%7C7K%7C7L/events/NowAndNext.json?optionalProperties=Channel.url%2CChannel.logoIMG%2CEvent.url&order=startDateTime&batchSize=2&batch=0';
 
 	return {
 
