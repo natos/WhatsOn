74df600f3370202fa026c543ab19462d36d2b04e
Natan <natan.santolo@gmail.com>
2012-02-16 16:16:01 +0100

    Added like button

--- public/js/main.js
+++ public/js/main.js
@@ -9,18 +9,11 @@ var wo = {
 require([	
 
 	'app'
+,	'//connect.facebook.net/es_LA/all.js#xfbml=1&appId=153316508108487'
 
 ], 
 function(App) {
 
 	App.initialize();
-
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=153316508108487";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
 
 });
