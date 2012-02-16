af9dc1b9e674068cc3c8b636c7700bc3cd548857
Natan <natan.santolo@gmail.com>
2012-02-16 16:00:55 +0100

    SaveNew user action: Watch tv show

--- public/js/views/UserActionView.js
+++ public/js/views/UserActionView.js
@@ -13,6 +13,7 @@ function() {
 	,	events: {
 
 			'click button.watch': 'watch'
+			'click button.share': 'share'
 
 		}
 
@@ -52,6 +53,27 @@ function() {
 			});
 
 		}
+
+	,	share: function(event) {
+
+			var url = $('meta[property="og:url"]').attr('content');
+			var title = $('meta[property="og:title"]').attr('content');
+			var description = $('meta[property="og:description"]').attr('content');
+
+			FB.ui({
+				method: 'feed'
+			,	link: url
+			,	picture: 'http://upcwhatson.herokuapp.com/assets/upclogo.jpg'
+			,	name: 'Share TV Show'
+			,	caption: title
+			,	description: description
+			}, 
+			function(reposne) {
+				console.log(reponse);
+			});
+
+		}
+
 
 	});
 
