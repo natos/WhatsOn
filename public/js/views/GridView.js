9897f61b96ab0b5ce0a0d0d6643f8625613125c0
Martin Sutherland <martin@sunpig.com>
2012-02-24 14:51:57 +0100

    Fix hour numbers in timeline bar

--- public/js/views/GridView.js
+++ public/js/views/GridView.js
@@ -94,22 +94,12 @@ function(Layer, GridSource) {
 	,	drawTimeLine: function() {
 
 			var self = this;
-			
-			var now = new Date();
 
-			var hour = now.getHours();
-
+			// Each <li> represents one hour
 			this['time-bar-list'].find('li').each(function(i, e) {
-
-				if ( ( hour + i ) > 23 ) {
-					now = new Date( now.valueOf() + 86400000 );
-					hour = now.getHours();
-				}
-
-				hour = hour + i;
-
-				$(e).html('<span>' + hour%24 + '</span>'); // FIX THIS!!!
-
+				console.log(i);
+				var hourTime = new Date(self.zeroTime.valueOf() + (i * (1000 * 60 * 60)));
+				$(e).html('<span>' + ('0' + hourTime.getHours().toString()).slice(-2) + '</span>');
 			});
 
 			timer.track('Draw Timeline');
