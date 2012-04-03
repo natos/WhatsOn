eeb8683fc1c220913673ad9f5cc2f43eedf96890
Natan <natan.santolo@gmail.com>
2012-04-03 18:01:57 +0200

    Cleaning up main.js

--- main.js
+++ main.js
@@ -12,31 +12,11 @@ requirejs.config({
 requirejs([
 
 	'controllers/app'
-,	'utils/metadata'
 
 ],
 
-function(App, Metadata) {
+function(App) {
 
 	new App();
-
-/*
-				// Meta data
-				var _metadata = [
-					{ property: "og:type"			, content: "upc-whatson:tv_channel" }
-				,	{ property: "og:url"			, content: "http://upcwhatson.herokuapp.com/channel/7A.html" }
-				,	{ property: "og:title"			, content: 'Channel' }
-				,	{ property: "og:description"	, content: 'Cool channel' }
-				,	{ property: "og:image"			, content: "http://upcwhatson.herokuapp.com/assets/upclogo.jpg" }
-				];
-
-				var __metadata = new Metadata();
-
-				console.log('metadata', __metadata.get());
-
-				__metadata.override(_metadata, 'property');
-
-				console.log('metadata', __metadata.get());
-*/
 
 });
