97187cbde68019f545997cb23eac01146e03b417
Natan <natan.santolo@gmail.com>
2012-05-02 18:30:39 +0200

    Improved Buffer calculations

---
+++ client/assets/js/components/buffer.js
@@ -0,0 +1,53 @@
+define([
+
+	'config/grid',
+	'components/channelbar'
+
+], function(g, ChannelBar) {
+
+/* private */
+
+// TODO: this should be refactor with the new Grid Model
+var clean = function() {
+
+	var events = $('.grid-event'), event, channel,
+
+	visibleChannels = ChannelBar.getSelectedChannels();
+
+	// to much elements out there
+	if ( events.length > g.MAX_DOM_ELEMENTS ) {
+
+		// each event in the dom
+		events.each(function(i, e) {
+			event = $(e);
+			channel = event.parent().attr('id').split('cc_').join('');
+			// is the channel not visible? remove the element
+			if ( $.inArray(channel, visibleChannels) === -1 ) {
+				event.remove();
+			}
+		});
+	}
+};
+
+/* @class ChannelBar */
+	Buffer = {};
+
+	/* constructor */
+	Buffer.initialize = function() {
+
+		g.$body.on(g.GRID_RENDERED, this.grooming);
+
+		return this;
+
+	};
+
+	Buffer.grooming = function() {
+
+		if (this.timer) { clearTimeout(this.timer); }
+
+		this.timer = setTimeout(clean, 300);
+	};
+
+	return Buffer;
+
+});
