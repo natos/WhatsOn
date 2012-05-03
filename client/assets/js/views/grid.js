f8251480f2d00b11722c92df81877303343b2ae9
Martin Sutherland <martin@sunpig.com>
2012-05-03 10:51:19 +0200

    Fix Safari date bug

--- client/assets/js/views/grid.js
+++ client/assets/js/views/grid.js
@@ -16,6 +16,20 @@ define([
 
 	var g = GridConfig,
 
+	/**
+	 * The EPG API returns dates (event.startDateTime, event.endDateTime)
+	 * in the format YYYY-MM-DDThh:mmZ. Safari can't use this string as a
+	 * parameters for a new Date constructor.
+	 */
+	parseApiDate = function(apiDate) {
+		var dt;
+		if (typeof(apiDate) === 'string') {
+			dt = new Date(apiDate.slice(0,4), parseInt(apiDate.slice(5,7),10) -1, parseInt(apiDate.slice(8,10),10), parseInt(apiDate.slice(11,13),10), parseInt(apiDate.slice(14,16),10));
+		}
+		return dt;
+	},
+
+
 	renderEvents = function(events) {
 
 		var link, description, i, event, width, left, startDateTime, endDateTime;
@@ -30,8 +44,8 @@ define([
 				return;
 			}
 
-			startDateTime = new Date(event.startDateTime);
-			endDateTime = new Date(event.endDateTime);
+			startDateTime = parseApiDate(event.startDateTime);
+			endDateTime = parseApiDate(event.endDateTime);
 
 			width = convert.timeToPixels(endDateTime, startDateTime);
 			left = convert.timeToPixels(startDateTime, g.ZERO);
