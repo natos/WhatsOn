56291604f078e3f43aab08deb5357a28c3a1c298
Natan <natan.santolo@gmail.com>
2012-07-27 18:39:05 +0200

    Removed unused code, and removed Swipe events@

--- client/js/components/carousel.js
+++ client/js/components/carousel.js
@@ -2,6 +2,7 @@
 * CarouselComponent
 * -----------------
 * @class Carousel
+* TODO: Swipe events to navigate the carousel content
 */
 
 define([
@@ -81,101 +82,6 @@ define([
 			$(this).find('.icon-play').removeClass('icon-play').addClass('icon-pause');
 		}
 		rowing = !rowing;
-	}
-
-	function addButtons() {
-
-		var playpause = $('<div>').attr('id','playpause').html('<i class="icon-pause">').on('click', playpauseHandler).appendTo(el),
-			navigator = $('<div>').addClass('navigator').appendTo(el),
-			disc = $('<i>').addClass('disc').addClass('icon-certificate'),
-			maxScreenWidth = Math.max($window.width(), $window.height()),
-			programme,
-			imgSize,
-			src;
-
-			if (maxScreenWidth <= 320) {
-				imgSize = 's';
-			} else if (maxScreenWidth <= 480) {
-				imgSize = 'm';
-			} else if (maxScreenWidth <= 1024) {
-				imgSize = 'l';
-			} else {
-				imgSize = 'xl';
-			}
-
-		list.find('.programme').each(function(i, e) {
-
-			programme = $(e);
-			programme.css({ 'left' : 100*i + '%' });
-
-			//programme.prepend('<img class="programme-bg" src="/assets/programmes/' + imgSize + '/' + coolPics[i] + '.jpg" />');
-
-			var $bg = programme.find('.programme-bg'),
-				src = $bg.attr('src');
-				src = src.replace('/s/', '/' + imgSize + '/');
-				$bg.attr('src', src);
-
-			map['disc-' + i] = disc
-				.clone()
-				.data('index', i)
-				.click(discHandler)
-				.appendTo(navigator);
-
-		});	
-
-		// select first
-		$('.disc').first().addClass('selected');
-
-		// add touch events
-		el.on('swipeRight swipeLeft', swipe);
-
-		// restart timer every disc click
-		$('.disc').bind('click', timer.restart );
-		
-	}
-
-	// swipe handler
-	function swipe(event) {
-
-		var bounce, disc = $('.disc.selected');
-
-		// restart timer
-		timer.restart();
-
-		switch(event.type) {
-
-			case "swipeRight": 
-				bounce = bounceRight;
-				disc = disc.prev();
-				break;
-			case "swipeLeft": 
-				bounce = bounceLeft;
-				disc = disc.next();
-				break;
-		}
-
-		if (!disc[0]) {
-			bounce();
-		} else { 
-			disc.trigger('click');
-		}
-
-	}
-
-	// bounce when the limit is reached
-	// for the rightswipe
-	function bounceRight() {
-		list.css('left', '5%');
-		setTimeout(function() {
-		list.css('left', '0%');
-		},300);
-	}
-	// for the leftswipe
-	function bounceLeft() {
-		list.css('left', '-905%');
-		setTimeout(function() {
-		list.css('left', '-900%');
-		},300);
 	}
 
 /* public */ 
