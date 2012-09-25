2d4796275767fe559f16304f5a26aaab3ced8609
Natan <natan.santolo@gmail.com>
2012-09-25 10:43:14 +0200

    EPGAPI Class clean and tidy.

--- client/js/utils/epgapi.js
+++ client/js/utils/epgapi.js
@@ -1,52 +1,14 @@
 define([
 
-	'modules/app',
-	'utils/convert',
-	'/js/lib/timetrack/timetrack.js'
+	'modules/app'
 
-], function(App, convert) {
+], function(App) {
 
 	'use strict';
 
-/*
-* Average time tool
-*/
-	//startShowingAverageTime();
-
-	var average_tilnow = 0,
-		average_time = 0,
-		average_count = 0,
-		last_average = 0;
-
-	function startShowingAverageTime() {
-		setTimeout(showAverageTime,1000);
-	}
-
-	function showAverageTime() {
-
-		setTimeout(showAverageTime,1000);
-
-		if (average_tilnow !== 0 && average_tilnow !== last_average) {
-			console.log('Average time <', average_tilnow, 'ms>');
-			last_average = average_tilnow;
-		}
-
-	}
-
-	function average(time) {
-		average_time += time; average_count += 1;
-		average_tilnow = Math.ceil(average_time / average_count);
-	}
-
 	// constants
-	var	HOURS_PER_SLICE = 6, // Hours per slice must be: 1, 2, 3, 4, 6, 8, 12, 24.
-		ESTIMATED_AVERAGE_EVENTS_PER_HOUR = 2,
-		EVENTS_PER_SLICE = ESTIMATED_AVERAGE_EVENTS_PER_HOUR * HOURS_PER_SLICE,
-		API_PREFIX = $('head').attr('data-api'),
-		$CUSTOM_EVENT_ROOT = App,
-		CHANNEL_EVENTS_RECEIVED_EVENT = 'grid:eventsReceived',
-		SEARCH_RESULTS_RECEIVED_EVENT = 'searchResultsReceived',
-		CACHE_DURATION = 60 * 24; // 1 day
+	var	API_PREFIX = $('head').attr('data-api'),
+		CHANNEL_EVENTS_RECEIVED_EVENT = 'grid:eventsReceived';
 
 
 	function getEventBatchFromAPI(channelsIds, timeSlice) {
@@ -86,7 +48,7 @@ define([
 
 		if (response) {
 
-			$CUSTOM_EVENT_ROOT.emit(CHANNEL_EVENTS_RECEIVED_EVENT, response, url);
+			App.emit(CHANNEL_EVENTS_RECEIVED_EVENT, response, url);
 			
 			if (response.nextBatchLink && response.nextBatchLink.href) {
 
