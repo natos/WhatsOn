0964d61867dda87f4cb213acc06447bacb3ce893
Martin Sutherland <martin@sunpig.com>
2012-10-08 14:46:20 +0200

    Add dynamically loaded mvc files to build config

--- client/js/app.build.js
+++ client/js/app.build.js
@@ -5,10 +5,10 @@
     modules: [
         {
             name: "main",
-            // Parts of the app are dynamically loaded, and 
+            // Parts of the app are dynamically loaded, and
             // are not automatically included by the r.js optimizer.
             // Specify these modules explicitly, so that they are
-            // included in the generated build file.
+            // included in the generated build file (main.js).
             include: [
                 // Core modules
                 "modules/user",
@@ -17,7 +17,18 @@
                 // Flaco MVC library
                 "lib/flaco/model",
                 "lib/flaco/controller",
-                "lib/flaco/view"
+                "lib/flaco/view",
+                // Application m/v/c files
+                "models/channel",
+                "models/nowandnext",
+                "models/search",
+                "models/grid",
+                "controllers/nowandnext",
+                "controllers/search",
+                "controllers/grid",
+                "views/nowandnext",
+                "views/search",
+                "views/grid"
             ]
         }
     ]
