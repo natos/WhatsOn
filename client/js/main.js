5873b26824151b3fb753143fe6d8b0b4f90ef61c
Natan <natan.santolo@gmail.com>
2012-10-15 11:17:28 +0200

    Added text and JSON requirejs plugin, to load JSON files remotly

--- client/js/main.js
+++ client/js/main.js
@@ -1,5 +1,20 @@
 require.config({
-/* ... */
+	
+	paths : {
+        //create alias to plugins (not needed if plugins are on the baseUrl)
+        text: 'lib/require-plugins/text',
+        //async: 'lib/requirejs-plugins/async',
+        //depend: 'lib/requirejs-plugins/depend',
+        //font: 'lib/requirejs-plugins/font',
+        //goog: 'lib/requirejs-plugins/goog',
+        //image: 'lib/requirejs-plugins/image',
+        json: 'lib/require-plugins/json',
+        //noext: 'lib/requirejs-plugins/noext',
+        //mdown: 'lib/requirejs-plugins/mdown',
+        //propertyParser : 'lib/requirejs-plugins/propertyParser',
+        //markdownConverter : 'lib/Markdown.Converter'
+    }
+
 });
 
 
