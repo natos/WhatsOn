452f996f67688f7555de6d2247bb49475997ed6c
Natan <natan.santolo@gmail.com>
2012-10-26 16:20:24 +0200

    Removed old code from search view

--- client/js/views/search.js
+++ client/js/views/search.js
@@ -62,87 +62,6 @@ define([
 
 	}
 
-	function filterHandler(event) {
-
-		event.stopPropagation();
-
-		var el = event.target,
-			checked = $(el).attr('checked'),
-			klass = $(el).parents('li').attr('class'),
-			value = el.value;
-
-		if (!klass||!value) { return; }
-
-		filters[klass][value] = checked;
-
-		applyFilters();
-
-	}
-
-	function isEmpty(klass) {
-		var empty_filters = true;
-		for (var filter in filters[klass]) {
-			if (filters[klass][filter]) {
-				empty_filters = false;
-			}
-		}
-		return empty_filters;
-	}
-
-	function applyFilters() {
-
-		var $results = $('#search-results'),
-			_emptyChannels = isEmpty('channel'),
-			_emptyDatetimes = isEmpty('datetime');
-	
-		if (_emptyChannels && _emptyDatetimes) {
-			$results.find("li").show();
-			return;
-		}
-	
-		$results.find("li").hide();
-		$results.find("li").each(function(index, item){
-			var $item = $(item);
-				$programme = $item.parents('.programme');
-	
-			if (_emptyChannels) {
-				if (filters.datetime[$item.data('datetime')]) {
-					$programme.show();
-					$item.show();
-				}
-			} else if (_emptyDatetimes) {
-				if (filters.channel[$item.data('channel')]) {
-					$programme.show();
-					$item.show();
-				}
-			} else {
-				if (filters.channel[$item.data('channel')] && filters.datetime[$item.data('datetime')]) {
-					$programme.show();
-					$item.show();
-				}
-			}
-		});
-	}
-
-	function initResults() {
-
-		// scan the possible filters
-		var $filters = $('#filters');
-		$filters.find('input').each(function(index, item) {
-
-			var klass = $(item).parents('li').attr('class'),
-				value = item.value;
-
-			if (!filters[klass]){
-				filters[klass] = {};
-			}
-			
-			filters[klass][value] = false;
-
-		});
-
-	}
-
 	function submitHandler(event) {
 		event.preventDefault();
 		var q = document.getElementById('q').value;
@@ -153,7 +72,8 @@ define([
 	function hidePageStructure() {
 		var searchContent = document.getElementById('search-content');
 		if (searchContent) {
-			searchContent.style.display = 'none';
+			//searchContent.style.display = 'none';
+			searchContent.parentNode.removeChild(searchContent);
 		}
 	}
 
@@ -308,17 +228,11 @@ define([
 
 /* public */
 
-	var filters = {};
-
 	function initialize() {
 
 		lang = new Language(app.selectedLanguageCode);
-		
-		window.addEventListener('click', filterHandler, false);
-				
-		window.addEventListener('change', filterHandler, false);
-				
-		window.addEventListener('submit', submitHandler);
+						
+		window.addEventListener('submit', submitHandler, false);
 
 		Event.on(searchConfig.MODEL_CHANGED, onSearchModelChanged);
 
@@ -330,7 +244,6 @@ define([
 
 		now = new Date();
 		renderPageStructure();
-		initResults();
 
 		return this;
 
@@ -339,10 +252,6 @@ define([
 	function finalize() {
 
 		hidePageStructure();
-		
-		window.removeEventListener('click', filterHandler, false);
-				
-		window.removeEventListener('change', filterHandler, false);
 				
 		window.removeEventListener('submit', submitHandler, false);
 
@@ -359,8 +268,7 @@ define([
 		name		: name,
 		initialize	: initialize,
 		finalize	: finalize,
-		render		: render,
-		filters		: filters
+		render		: render
 	});
 
 });
